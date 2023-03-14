6cfbc581ae7432077a3c0c4dcda716c4e23049f4
Anders Flotten <anders3835@hotmail.com>
2023-03-14 05:53:28 +0000

    Implement select problems

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,25 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey",  annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# loop is only outputting Calgary (changed from burnaby), Whistler and Yaletown. Gastown was deleted in previous exercise 
+@mens_stores.all.each do |store|
+  puts store.name
+end
+
+#womens appearal & less than $1M revenue
+@womens_stores = Store.where(womens_apparel: true)
+
+@womens_stores.all.each do |store|
+  if store.annual_revenue < 1000000
+  puts store.name
+  end
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "----TOTAL REVENUE----"
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue #4114000
+
+puts "----Average Revenue----"
+@average_revenue = @total_revenue/Store.count
+puts @average_revenue #822800
+
+puts "Count of stores with a million or more in rev"
+puts Store.where("annual_revenue > 1000000").count.to_s
